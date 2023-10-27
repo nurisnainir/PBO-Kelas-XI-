ac40ef323f774f4cad9e2e537c3c930d370b8f57
nurisnainir <143146259+nurisnainir@users.noreply.github.com>
2023-10-27 08:03:27 +0700

    Add files via upload

---
+++ cetakdatasiswa.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ex1;
+
+/**
+ *
+ * @author LAB1PC16
+ */
+public class cetakdatasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = " asty meilista";
+        int absen = 03;
+        int kelas = 11;
+        double nilaiMatematika = 100;
+        double nilaiBahasaIndonesia = 95;
+        double nilaiPBO = 90;
+
+        //Output
+        System.out.println("nama = " + nama);
+        System.out.println("absen = " + absen);
+        System.out.println("kelas = " + kelas);
+        System.out.println("nilai matematika = " + nilaiMatematika);
+        System.out.println("nilai bahasa indonesia = " + nilaiBahasaIndonesia);
+        System.out.println("nilai pbo = " + nilaiPBO);
+       
+    }
+    
+    
+}                   
